90975b9da25f6f93b3bc1874035ae7d3a5b97dba
Empire201 <norbertcserpnyak16@gmail.com>
2021-11-12 05:31:55 +0100

    Improvements, developments.

--- src/com/norbertcserpnyak16/Main.java
+++ src/com/norbertcserpnyak16/Main.java
@@ -1,23 +1,23 @@
 package com.norbertcserpnyak16;
-
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.print("Adjon meg egy értékét: ");
         int a = new Scanner(System.in).nextInt();
         System.out.print("Adjon meg mégegy értékét: ");
         int b = new Scanner(System.in).nextInt();
 
-        int i = a + b;
-        int i1 = a - b;
-        int i2 = a * b;
-        int i3 = a / b;
-        System.out.println("Összeadás: " + i);
-        System.out.println("Kivonás: " + i1);
-        System.out.println("Szorzás: " + i2);
-        System.out.println("Osztás: " + i3);
+        int osszeadas = a + b;
+        int kivonas = a - b;
+        int szorzas= a * b;
+        int osztas = a / b;
+
+        System.out.println("Összeadás eredménye: " + osszeadas);
+        System.out.println("Kivonás eredménye: " + kivonas);
+        System.out.println("Szorzás eredménye: " + szorzas);
+        System.out.println("Osztás eredménye: " + osztas);
 
     }
 }
